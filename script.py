303dfe238bcfc71190c21e53e1b46b4d7f72b3bb
Talha Anwar <chtalhaanwar@gmail.com>
2021-06-02 21:06:50 +0500

    script added

---
+++ script.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 16:31:40 2021
+
+@author: TAC
+"""
+
+from glob import glob
+import pandas as pd
+import numpy as np
+data=[]
+for csv in glob('submissions/English/*.csv'):
+    data.append(pd.read_csv(csv).iloc[:,-1])
+    
+mean=np.mean(data,0)
+mean=np.where(mean>0.5,1,0)
+df=pd.read_csv(csv)
+df.type=mean
+import os 
+os.makedirs('submissions/English/output')
+
+for row in df.index:
+    xml = []
+    xml.append('<author id="{}"'.format(df.loc[row,'author-id']))
+    xml.append('lang="{}"'.format(df.loc[row,'lang']))
+    xml.append('type="{}"'.format(df.loc[row,'type']))
+    xml.append('/>')
+    x='\n'.join(xml)
+    myfile = open("submissions/English/output/{}.xml".format(df.loc[row,'author-id']), "w")
+    myfile.write(x)
+    myfile.close()
